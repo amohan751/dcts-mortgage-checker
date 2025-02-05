0ee97d80ee1c6df05fe15001e0d83a50c24acae8
amohan751 <abhijith.or.mohan@gmail.com>
2025-02-05 10:32:35 +0530

    Initial commit of mortymeter application

---
+++ src/main/java/com/amohan/mortymeter/MortymeterApplication.java
@@ -0,0 +1,13 @@
+package com.amohan.mortymeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortymeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortymeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amohan/mortymeter/MortymeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amohan.mortymeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortymeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
